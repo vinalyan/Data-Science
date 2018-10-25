30dccab6bf4698dbf80be7d47fecb533be4d11dc
Christian Urcuqui <rhapsody_cris@hotmail.com>
2018-10-25 00:54:01 -0500

    updated

--- Machine_learning/Reinforcement Learning/openai_gym.py
+++ Machine_learning/Reinforcement Learning/openai_gym.py
@@ -1,10 +1,10 @@
 import gym
 
 
-#env = gym.make('MountainCar-v0')
+env = gym.make('MountainCar-v0')
 #env = gym.make('CartPole-v0')
 #env = gym.make('HandManipulateBlock-v0')
-env = gym.make('FrozenLake-v0')
+#env = gym.make('FrozenLake-v0')
 env.reset()
 for _ in range(1000):
     env.render()
