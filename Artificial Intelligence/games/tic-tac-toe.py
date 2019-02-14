52910b9894783ecb47bc7ef11d15bc4bda58ece8
Christian Urcuqui <rhapsody_cris@hotmail.com>
2019-02-13 22:15:47 -0500

    updated

--- Artificial Intelligence/games/tic-tac-toe.py
+++ Artificial Intelligence/games/tic-tac-toe.py
@@ -1,12 +1,14 @@
 # tres en raya con algoritmo minimax
 
 import sys
+from operator import itemgetter
 
 MAX = 1
 MIN = -1
 
 global jugada_maquina
 
+# metodo de minmax para generar las distintas opciones y seleccionar la mejor jugada 
 def minimax(tablero, jugador):
     global jugada_maquina
     
@@ -17,19 +19,22 @@ def minimax(tablero, jugador):
     # generar las posibles jugadas
     movimientos = []
     for jugada in range(0, len(tablero)):
+        # si faltan jugadas por realizar en la posición entonces...
         if tablero[jugada] == 0:
+            # creamos un tablero copia auxiliar y le asignamos el valor dependiendo del tipo de jugador (humano/ordenador)
             tableroaux = tablero[:]
             tableroaux[jugada] = jugador
-
+            # creamos las jugadas para el jugador adversario con el nuevo valor
             puntuacion = minimax(tableroaux, jugador * (-1))
             movimientos.append([puntuacion, jugada])
-
+    
+    # si el turno es del ordenador obtenemos el movimiento con el mayor valor y si es del humano obtenemos el de menor valor 
     if jugador == MAX:
-        movimiento = max(movimientos)
+        movimiento = max(movimientos, key=str)        
         jugada_maquina = movimiento[1]
         return movimiento 
-    else:
-        movimiento = min(movimiento)
+    else:       
+        movimiento = min(movimientos, key=str)        
         return movimiento[0]
 
 def game_over(tablero):
@@ -47,6 +52,7 @@ def game_over(tablero):
 
     
 def ganador(tablero):
+    # combinaciones de estados de ganadores
     lineas = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5,8], [0, 4, 8], [2, 4, 6]]
     ganador = 0
     for linea in lineas:
@@ -54,28 +60,45 @@ def ganador(tablero):
             ganador = tablero[linea[0]]
     return ganador
 
+# metodo que imprime el trablero de juego 
 def ver_tablero(tablero):
-    for i in range(0,3):
-        for j in range(0,3):
-            if tablero[i*3+j] == MAX:
-                print('X')
-            elif tablero[i*3+j] == MIN:
-                print('O')
-            else:
-                print('.')
-        print('')
+    
+    board = list(map(str, tablero))    
+    for i in range(0, len(tablero)):
+        if tablero[i] == MAX:
+            board[i] = 'X'
+        elif tablero[i] == MIN:
+            board[i] = 'O'
+        else:
+            board[i] = ' '
+  
+    print('   |   |')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print('   |   |')    
 
+# metodo encargado de procesar el movimiento del jugador humano
 def juega_humano(tablero):
-    ok=False
+    ok= False
     while not ok:
         casilla = input("Casilla?")
-        if str(casilla) in '0123456789' and len(str(casilla)) == 1 and tablero[casilla-1] == 0:
-            if casilla == 0:
-                sys.exit(0)
-            tablero[casilla-1] = MIN
+        # obtenemos la posición de la casilla de 1-9 y comparamos con su respectivo indice en la lista
+        if str(casilla) in '0123456789' and len(str(casilla)) == 1 and tablero[int(casilla)-1] == 0:
+            # asignamos a la casilla del jugador un valor de -1
+            tablero[int(casilla)-1] = MIN
             ok = True
+        if casilla == "exit":
+            sys.exit(0)
     return tablero
 
+# metodo del ordenador donde se aplica min-max con la propagación y selección de la mejor opción de la jugada teniendo la función de evaluación
 def juega_ordenador(tablero):
     global jugada_maquina
     punt = minimax(tablero[:], MAX)
@@ -83,17 +106,19 @@ def juega_ordenador(tablero):
     return tablero
 
 if __name__ == "__main__":
-    print("Introduce casilla o 0 para terminar")
+    print("Introduce casilla o exit para terminar")
     tablero = [0,0,0,0,0,0,0,0,0]
+    
     while(True):
         ver_tablero(tablero)
         tablero = juega_humano(tablero)
         if game_over(tablero):
             break
         
-        tablero = juega_humano(tablero)
+        tablero = juega_ordenador(tablero)
         if game_over(tablero):
             break
+            
     ver_tablero(tablero)
     g = ganador(tablero)
     if g == 0:
