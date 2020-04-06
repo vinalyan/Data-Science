9ac40c0e4bc14dfc34e8909df62d782e15ac6592
Christian Urcuqui <rhapsody_cris@hotmail.com>
2020-04-05 19:33:32 -0500

    updated

--- Generative Models/models/AE.py
+++ Generative Models/models/AE.py
@@ -6,7 +6,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint 
 from keras.utils import plot_model
 
-from utils.callbacks import CustomCallback, step_decay_schedule 
+from utils.callbacks import CustomCallback, step_decay_schedule
 
 import numpy as np
 import json
@@ -14,7 +14,7 @@ import os
 import pickle
 
 
-class VariationalAutoencoder():
+class Autoencoder():
     def __init__(self
         , input_dim
         , encoder_conv_filters
@@ -25,10 +25,10 @@ class VariationalAutoencoder():
         , decoder_conv_t_strides
         , z_dim
         , use_batch_norm = False
-        , use_dropout= False
+        , use_dropout = False
         ):
 
-        self.name = 'variational_autoencoder'
+        self.name = 'autoencoder'
 
         self.input_dim = input_dim
         self.encoder_conv_filters = encoder_conv_filters
@@ -48,7 +48,7 @@ class VariationalAutoencoder():
         self._build()
 
     def _build(self):
-        
+
         ### THE ENCODER
         encoder_input = Input(shape=self.input_dim, name='encoder_input')
 
@@ -65,10 +65,10 @@ class VariationalAutoencoder():
 
             x = conv_layer(x)
 
+            x = LeakyReLU()(x)
+
             if self.use_batch_norm:
                 x = BatchNormalization()(x)
-
-            x = LeakyReLU()(x)
 
             if self.use_dropout:
                 x = Dropout(rate = 0.25)(x)
@@ -76,24 +76,12 @@ class VariationalAutoencoder():
         shape_before_flattening = K.int_shape(x)[1:]
 
         x = Flatten()(x)
-        self.mu = Dense(self.z_dim, name='mu')(x)
-        self.log_var = Dense(self.z_dim, name='log_var')(x)
-
-        self.encoder_mu_log_var = Model(encoder_input, (self.mu, self.log_var))
-
-        def sampling(args):
-            mu, log_var = args
-            epsilon = K.random_normal(shape=K.shape(mu), mean=0., stddev=1.)
-            return mu + K.exp(log_var / 2) * epsilon
-
-        encoder_output = Lambda(sampling, name='encoder_output')([self.mu, self.log_var])
+        encoder_output= Dense(self.z_dim, name='encoder_output')(x)
 
         self.encoder = Model(encoder_input, encoder_output)
-        
-        
+
 
         ### THE DECODER
-
         decoder_input = Input(shape=(self.z_dim,), name='decoder_input')
 
         x = Dense(np.prod(shape_before_flattening))(decoder_input)
@@ -111,47 +99,36 @@ class VariationalAutoencoder():
             x = conv_t_layer(x)
 
             if i < self.n_layers_decoder - 1:
+                x = LeakyReLU()(x)
+                
                 if self.use_batch_norm:
                     x = BatchNormalization()(x)
-                x = LeakyReLU()(x)
+                
                 if self.use_dropout:
                     x = Dropout(rate = 0.25)(x)
             else:
                 x = Activation('sigmoid')(x)
 
-            
-
         decoder_output = x
 
         self.decoder = Model(decoder_input, decoder_output)
 
-        ### THE FULL VAE
+        ### THE FULL AUTOENCODER
         model_input = encoder_input
         model_output = self.decoder(encoder_output)
 
         self.model = Model(model_input, model_output)
 
 
-    def compile(self, learning_rate, r_loss_factor):
+    def compile(self, learning_rate):
         self.learning_rate = learning_rate
 
-        ### COMPILATION
-        def vae_r_loss(y_true, y_pred):
-            r_loss = K.mean(K.square(y_true - y_pred), axis = [1,2,3])
-            return r_loss_factor * r_loss
-
-        def vae_kl_loss(y_true, y_pred):
-            kl_loss =  -0.5 * K.sum(1 + self.log_var - K.square(self.mu) - K.exp(self.log_var), axis = 1)
-            return kl_loss
-
-        def vae_loss(y_true, y_pred):
-            r_loss = vae_r_loss(y_true, y_pred)
-            kl_loss = vae_kl_loss(y_true, y_pred)
-            return  r_loss + kl_loss
-
         optimizer = Adam(lr=learning_rate)
-        self.model.compile(optimizer=optimizer, loss = vae_loss,  metrics = [vae_r_loss, vae_kl_loss])
 
+        def r_loss(y_true, y_pred):
+            return K.mean(K.square(y_true - y_pred), axis = [1,2,3])
+
+        self.model.compile(optimizer=optimizer, loss = r_loss)
 
     def save(self, folder):
 
@@ -177,20 +154,21 @@ class VariationalAutoencoder():
 
         self.plot_model(folder)
 
+        
+
 
     def load_weights(self, filepath):
         self.model.load_weights(filepath)
 
+    
     def train(self, x_train, batch_size, epochs, run_folder, print_every_n_batches = 100, initial_epoch = 0, lr_decay = 1):
 
         custom_callback = CustomCallback(run_folder, print_every_n_batches, initial_epoch, self)
         lr_sched = step_decay_schedule(initial_lr=self.learning_rate, decay_factor=lr_decay, step_size=1)
-        
-        checkpoint_filepath=os.path.join(run_folder, "weights/weights-{epoch:03d}-{loss:.2f}.h5")
-        checkpoint1 = ModelCheckpoint(checkpoint_filepath, save_weights_only = True, verbose=1)
+
         checkpoint2 = ModelCheckpoint(os.path.join(run_folder, 'weights/weights.h5'), save_weights_only = True, verbose=1)
 
-        callbacks_list = [checkpoint1, checkpoint2, custom_callback, lr_sched]
+        callbacks_list = [checkpoint2, custom_callback, lr_sched]
 
         self.model.fit(     
             x_train
@@ -202,32 +180,6 @@ class VariationalAutoencoder():
             , callbacks = callbacks_list
         )
 
-
-
-    def train_with_generator(self, data_flow, epochs, steps_per_epoch, run_folder, print_every_n_batches = 100, initial_epoch = 0, lr_decay = 1, ):
-
-        custom_callback = CustomCallback(run_folder, print_every_n_batches, initial_epoch, self)
-        lr_sched = step_decay_schedule(initial_lr=self.learning_rate, decay_factor=lr_decay, step_size=1)
-
-        checkpoint_filepath=os.path.join(run_folder, "weights/weights-{epoch:03d}-{loss:.2f}.h5")
-        checkpoint1 = ModelCheckpoint(checkpoint_filepath, save_weights_only = True, verbose=1)
-        checkpoint2 = ModelCheckpoint(os.path.join(run_folder, 'weights/weights.h5'), save_weights_only = True, verbose=1)
-
-        callbacks_list = [checkpoint1, checkpoint2, custom_callback, lr_sched]
-
-        self.model.save_weights(os.path.join(run_folder, 'weights/weights.h5'))
-                
-        self.model.fit_generator(
-            data_flow
-            , shuffle = True
-            , epochs = epochs
-            , initial_epoch = initial_epoch
-            , callbacks = callbacks_list
-            , steps_per_epoch=steps_per_epoch 
-            )
-
-
-    
     def plot_model(self, run_folder):
         plot_model(self.model, to_file=os.path.join(run_folder ,'viz/model.png'), show_shapes = True, show_layer_names = True)
         plot_model(self.encoder, to_file=os.path.join(run_folder ,'viz/encoder.png'), show_shapes = True, show_layer_names = True)
